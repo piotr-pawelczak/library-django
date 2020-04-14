ded543b7c95fd842b37a554a4d99c3a772450fc0
piotr-pawelczak <pecio@student.agh.edu.pl>
2020-04-14 20:39:58 +0200

    Add database, model - Book and create superusers

--- library_django/library/models.py
+++ library_django/library/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=300, default='')
+    year = models.IntegerField(default=0)
+    publisher = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100)
+
+    time = models.IntegerField(default=14)
+    is_available = models.BooleanField(default=True)
+    edition = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f'"{self.title}", {self.author}'
+
+# book = Book(title="Język C++ Szkoła Programowania", authors=["Stephen Prata"], year=2013, publisher="Helion", genre="Science", edition=6)
