813c474cf09f8528c797817ca1a05f53a0821ad3
piotr-pawelczak <pecio@student.agh.edu.pl>
2020-04-24 18:44:02 +0200

    Add scroll function

--- library_django/library_django/settings.py
+++ library_django/library_django/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'library_django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,6 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
