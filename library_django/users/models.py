49e811a56c8a99d00ae8e6d1a32111263d35d014
piMateusz <pilecki.mat@gmail.com>
2020-04-26 21:14:54 +0200

    conflicts are coming

--- library_django/users/models.py
+++ library_django/users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 import datetime
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Profile(models.Model):
@@ -11,12 +12,14 @@ class Profile(models.Model):
     street = models.CharField(max_length=100, default='')
     city = models.CharField(max_length=80, default='')
     post_code = models.CharField(max_length=6, default='')
-    phone_number = models.IntegerField(default=0)
+    # phone_number = PhoneNumberField(unique=True)
+    phone_number = models.CharField(max_length=9, default='')
     country = models.CharField(max_length=10, default='')
     birth_date = models.DateField(default=datetime.date.today)
     profile_picture = models.ImageField(default='static/library/images/news.jpg', upload_to='static/library/images/profile_pictures')
     rules_agreement = models.BooleanField(default=False)
     pesel = models.IntegerField(default=0)
+
 
     def __str__(self):
         return f"{self.user.username} profile"
