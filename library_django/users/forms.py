ae6bd05e1c3bd9cd2b6b96f461bfaf5b4655b44e
piMateusz <pilecki.mat@gmail.com>
2020-04-27 12:29:30 +0200

    DK what has changed since yesterday but commit anyway

--- library_django/users/forms.py
+++ library_django/users/forms.py
@@ -14,19 +14,19 @@ class RegisterForm(UserCreationForm):
     surname = forms.CharField()
     street = forms.CharField()
     city = forms.CharField()
-    post_code = forms.CharField(validators=[RegexValidator(regex=re.compile(r'\d{2}-\d{3}'), message='Enter valid '
+    post_code = forms.CharField(validators=[RegexValidator(regex=re.compile(r'^\d{2}-\d{3}$'), message='Enter valid '
                                                                                                      'post-code '
                                                                                                      'XX-XXX')])
 
     # post_code = forms.CharField(validators=validate_post_code)
     # phone_number = PhoneNumberField()
-    phone_number = forms.CharField(validators=[RegexValidator(regex=re.compile(r'\d{9}'), message='Enter valid phone number')])
+    phone_number = forms.CharField(validators=[RegexValidator(regex=re.compile(r'^\d{9}$'), message='Enter valid phone number')])
 
     # Get phone as string from object field: user.phone.as_e164
     country = forms.CharField()
     birth_date = forms.DateField()
     profile_picture = forms.ImageField(required=False)
-    pesel = forms.CharField(validators=[RegexValidator(regex=re.compile(r'\d{11}'), message='Enter valid pesel')])
+    pesel = forms.CharField(validators=[RegexValidator(regex=re.compile(r'^\d{11}$'), message='Enter valid pesel')])
     rules_agreement = forms.BooleanField(label=mark_safe('I have read and agree to the <a href="#">Terms and '
                                                          'Conditions</a> and <a href="#">Privacy Policy</a>'))
 
