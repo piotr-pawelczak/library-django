1021ed876949af3b4a4634eeaad5341a58d476fc
piMateusz <pilecki.mat@gmail.com>
2020-04-29 19:03:44 +0200

    Almost all class views fixed

--- library_django/library/urls.py
+++ library_django/library/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, include
 from . import views
-from .views import ArticleDetailView, ArticleCreateView
+from .views import ArticleListView, ArticleDetailView, ArticleCreateView, ArticleUpdateView, ArticleDeleteView
 
 urlpatterns = [
-    path('', views.home, name="library-home"),
+    path('', ArticleListView.as_view(), name="library-home"),
+    path('article/<int:pk>/', ArticleDetailView.as_view(), name='article-detail'),
+    path('article/new/', ArticleCreateView.as_view(), name='new-article'),
+    path('article/<int:pk>/update/', ArticleUpdateView.as_view(), name='article-update'),
+    path('article/<int:pk>/delete', ArticleDeleteView.as_view(), name='article-delete'),
     path('books/', views.books, name="library-books"),
     path('about/', views.about, name="library-about"),
     path('contact/', views.contact, name="library-contact"),
-    path('article/<int:pk>', ArticleDetailView.as_view(), name='article-detail'),
-    path('article/new', ArticleCreateView.as_view(), name='new-article'),
 ]
--- library_django/library/views.py
+++ library_django/library/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Article
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
 
 def home(request):
@@ -15,22 +16,23 @@ def home(request):
     return render(request, 'library/home.html', {'articles': articles})
 
 # FIXME overriding home method with class ArticleListView may not be beneficial (consider it)
-# class ArticleListView(ListView):
-#     model = Article
-#     template_name = 'library/home.html'
-#     context_object_name = 'articles'
-#
-#     def get_context_data(self, *, object_list=None, **kwargs):
-#         context = super(ArticleListView, self).get_context_data(**kwargs)
-#         articles = []
-#
-#         for article in Article.objects.all():
-#             content = article.content
-#             if len(content) > 500:
-#                 content = article.content[0:500] + "..."
-#             articles.append((article, content))
-#             context = {'articles': articles}
-#         return context
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'library/home.html'
+    context_object_name = 'articles'
+    ordering = ['-date_posted']
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(ArticleListView, self).get_context_data(**kwargs)
+        articles = []
+
+        for article in Article.objects.all():
+            content = article.content
+            if len(content) > 500:
+                content = article.content[0:500] + "..."
+            articles.append((article, content))
+            context = {'articles': articles}
+        return context
 
 
 class ArticleDetailView(DetailView):
@@ -39,23 +41,36 @@ class ArticleDetailView(DetailView):
     context_object_name = 'article'
 
 
-class ArticleCreateView(CreateView):
+class ArticleCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
     model = Article
     fields = ['title', 'content', 'image']
-
-    # template_name = article_form.html
+    permission_required = 'user.is_staff'
+    # permission_denied_message = 'Permission denied'
+    template_name = 'library/article_form.html'
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
 
-class ArticleUpdateView(UpdateView):
-    pass
+class ArticleUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
+    model = Article
+    fields = ['title', 'content', 'image']
+    permission_required = 'user.is_staff'
+    # permission_denied_message = 'Permission denied'
+    template_name = 'library/article_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
-class ArticleDeleteView(DeleteView):
-    pass
+class ArticleDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
+    model = Article
+    template_name = 'library/article-delete.html'
+    permission_required = 'user.is_staff'
+    # permission_denied_message = 'Permission denied'
+    success_url = '/'
 
 
 def books(request):
